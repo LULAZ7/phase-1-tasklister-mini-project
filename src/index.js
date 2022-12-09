d8f3b35f9a15e21f2b1cd3452dc2db9c1f929628
Lula <loulazakaria02@gmail.com>
2022-12-09 11:40:58 +0300

    Complted assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
 });
+let form = document.querySelector('form');
+
+  form.addEventListener("submit", (event)=>{
+     event.preventDefault();
+     let input = document.getElementById('new-task-description').value;
+      append(input);
+      form.reset();
+  })
+
+const append = (input) => {
+  let description= document.getElementById("tasks");
+  let p = document.createElement("p");
+  p.className += " " + "mt-3";
+  p.textContent = input
+  let row = description.appendChild(p)
+
+
+  let btn = document.createElement("button");
+  btn.className += " " + "btn btn-xm btn-danger ml-5";
+  btn.textContent = "X";
+  row.appendChild(btn)
+
+  btn.onclick = () => (delete_(p)) ;
+
+}
+
+const delete_ = (p) => {
+  p.remove();
+} 
